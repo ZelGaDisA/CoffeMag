c9025e7f858faf1f2f3d9c9041ecf877649d6e9f
ZelGaDisA <zelasas@mail.ru>
2021-10-21 22:39:53 +0300

    add Blog and fix Home

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,11 +1,93 @@
 import React, { Component } from "react"
+import {
+    Media,
+    Container,
+    Row,
+    Col,
+    Card,
+    ListGroup
+} from "react-bootstrap"
+
 
 export default class Blog extends Component {
     render() {
         return (
-            <div>
-                Blog
-            </div>
+            <Container>
+                <Row>
+                    <Col md="9">
+                        <Media className="m-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src="https://media.wired.com/photos/5f4fff2f846cd1b8a71081e9/1:1/w_1800,h_1800,c_limit/Gear-8718800_OXO-Brew-8-Cup-Coffee-Maker_Profile_Bubbling.jpg"
+                            />
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>
+                                    Lorem pidori blyat text eby4ii kak on menia zaebal no 4to podelat esli
+                                    y4ast takaia kak ia sebe i predtavlial, takie sydbi y programmistow
+                                    ia nadeys vi menya ponimaete i ne bydete 4itat etot text.
+                                </p>
+                            </Media.Body>
+                        </Media>
+                        <Media className="m-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src="https://media.wired.com/photos/5f4fff2f846cd1b8a71081e9/1:1/w_1800,h_1800,c_limit/Gear-8718800_OXO-Brew-8-Cup-Coffee-Maker_Profile_Bubbling.jpg"
+                            />
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>
+                                    Lorem pidori blyat text eby4ii kak on menia zaebal no 4to podelat esli
+                                    y4ast takaia kak ia sebe i predtavlial, takie sydbi y programmistow
+                                    ia nadeys vi menya ponimaete i ne bydete 4itat etot text.
+                                </p>
+                            </Media.Body>
+                        </Media>
+                        <Media className="m-5">
+                            <img
+                                width={150}
+                                height={150}
+                                className="mr-3"
+                                src="https://media.wired.com/photos/5f4fff2f846cd1b8a71081e9/1:1/w_1800,h_1800,c_limit/Gear-8718800_OXO-Brew-8-Cup-Coffee-Maker_Profile_Bubbling.jpg"
+                            />
+                            <Media.Body>
+                                <h5>Blog post</h5>
+                                <p>
+                                    Lorem pidori blyat text eby4ii kak on menia zaebal no 4to podelat esli
+                                    y4ast takaia kak ia sebe i predtavlial, takie sydbi y programmistow
+                                    ia nadeys vi menya ponimaete i ne bydete 4itat etot text.
+                                </p>
+                            </Media.Body>
+                        </Media>
+                    </Col>
+                    <Col md="3">
+                        <h5 className="text-center">Categiries</h5>
+                        <Card>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>Зерна</ListGroup.Item>
+                                <ListGroup.Item>Помолка</ListGroup.Item>
+                                <ListGroup.Item>Варка</ListGroup.Item>
+                                <ListGroup.Item>Оформление</ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+
+                        <Card className="mt-3" bg="light">
+                            <Card.Body>
+                                <Card.Title>Side vision</Card.Title>
+                                <Card.Text>
+                                    Lorem pidori blyat text eby4ii kak on menia zaebal no 4to podelat esli
+                                    y4ast takaia kak ia sebe i predtavlial, takie sydbi y programmistow
+                                    ia nadeys vi menya ponimaete i ne bydete 4itat etot text.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,58 @@
 import React, { Component } from "react"
+import { Button, Container, CardDeck, Card } from "react-bootstrap"
 import CarouselBox from "../Components/CarouselBox"
 
 export default class Home extends Component {
     render() {
         return (
-            <CarouselBox />
+            <>
+                <CarouselBox />
+
+                <Container>
+                    <h2 className="text-center m-4">Our Work</h2>
+                    <CardDeck className="m-4">
+                        <Card>
+                            <Card.Img
+                                variant="top"
+                                src="http://restoranoff.ru/upload/iblock/54b/osnova-effektivnoy-komandy_main.jpg"
+                            />
+                            <Card.Body>
+                                <Card.Title>Founders</Card.Title>
+                                <Card.Text>
+                                    We a the bes of CoffeMakers at this wold motherfuker babyes.
+                                </Card.Text>
+                                <Button variant="primary"> CoffeMakers </Button>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img
+                                variant="top"
+                                src="https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_04/3444881/210124-bestcoffee-bd-2x1still.jpg"
+                            />
+                            <Card.Body>
+                                <Card.Title>Founders</Card.Title>
+                                <Card.Text>
+                                    We a the bes of CoffeMakers at this wold motherfuker babyes.
+                                </Card.Text>
+                                <Button variant="primary"> CoffeMakers </Button>
+                            </Card.Body>
+                        </Card>
+                        <Card>
+                            <Card.Img
+                                variant="top"
+                                src="https://www.techadvisor.com/cmsdata/slideshow/3691237/best_coffee_maker_2021_thumb800.jpg"
+                            />
+                            <Card.Body>
+                                <Card.Title>Founders</Card.Title>
+                                <Card.Text>
+                                    We a the bes of CoffeMakers at this wold motherfuker babyes.
+                                </Card.Text>
+                                <Button variant="primary"> CoffeMakers </Button>
+                            </Card.Body>
+                        </Card>
+                    </CardDeck>
+                </Container>
+            </>
         )
     }
 }
